e2d30a246383c2a728e3a98db1970d18df6e861e
Shilpy <shilpy42542s@gmail.com>
2024-06-09 01:15:47 +0530

    Create Server -> build express app with middleware -> in Utils create connection with database and handle error - > Write middlewares for async error handling etc -> Make Model (Defining Schema) -> Write Controller(Logical Part)

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,35 @@
-import express from "express";
-
+import express, { Request, Response, NextFunction } from "express";
 export const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+require('dotenv').config();
+import { ErrorMiddleware } from "./middleware/error";
+
+
+// body parser - for Cloudinary
+app.use(express.json({limit: "50mb"}));
+
+// cookie-parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN, 
+}));
+
+// testing API
+app.get("/test", (req:Request , res:Response, next:NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working"
+    })
+})
+
+// unknown route
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`)
+    res.send(404).send();
+    next(err)
+})
+
+app.use(ErrorMiddleware)
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import { app } from "./app";
 require("dotenv").config();
+import connectDB from "./utils/db";
 
 
 // create server
 app.listen(process.env.PORT, () => {
     console.log(` Server is connected with port ${process.env.PORT} `);
+    connectDB()
 })
