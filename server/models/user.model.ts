5ececeeb4c261a95160fe04c57bb114cb66628d0
Shilpy <shilpy42542s@gmail.com>
2024-06-11 02:18:59 +0530

    SignAccessToken/SignRefreshToken

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,5 +1,7 @@
+require('dotenv').config();
 import mongoose, {Document, Model, Schema} from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,6 +17,8 @@ export interface IUser extends Document {
     isVerified: boolean;
     courses: Array<{courseId: string}>;
     comparePassword: (password: string) => Promise<boolean>;
+    SignAcessToken: () => string;
+    SignRefreshToken: () => string;
 };
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
@@ -67,6 +71,16 @@ userSchema.pre<IUser>('save', async function(next){
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
+
+// sign access token
+userSchema.methods.SignAcessToken = function () {
+    return jwt.sign({id: this._id}, process.env.ACCESS_TOKEN || '');
+}
+
+// sign refresh token
+userSchema.methods.SignRefreshToken = function () {
+    return jwt.sign({id: this._id}, process.env.REFRESH_TOKEN || '');
+}
 
 // compare password
 userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
